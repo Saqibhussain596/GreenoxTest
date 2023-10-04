614991e4f3433efc19065f4b9d518574e679aa54
Saqib Hussain <saqibhussain596@gmail.com>
2023-10-04 23:29:32 +0530

    navbar created

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import "./Burger.css"
+import React, { useState } from 'react';
+import './Burger.css';
+
 function Burger() {
+  const [isOpen, setIsOpen] = useState(false); // Initial state is closed
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Toggle the open/close state
+  };
+
   return (
-    <div className="burger">
-      <div className="line1"></div>
-      <div className="line2"></div>
+    <div className={`navbar ${isOpen ? 'open' : ''}`}>
+      <div className="burger" onClick={toggleMenu}>
+        <div className={`line1 ${isOpen ? 'open' : ''}`}></div>
+        <div className={`line2 ${isOpen ? 'open' : ''}`}></div>
+      </div>
+      <div className={`menu ${isOpen ? 'open' : ''}`}>
+        <ul>
+          <div className="close-btn" onClick={toggleMenu}>
+            Close <i className="fa fa-close"></i>
+          </div>
+          <li>Home</li>
+          <li>Solutions <i className="fa fa-angle-down"></i></li>
+          <li>Creative Gallery<i className="fa fa-angle-down"></i></li>
+          <li>Insights<i className="fa fa-angle-down"></i></li>
+          <li>About<i className="fa fa-angle-down"></i></li>
+          <li>Contact</li>
+        </ul>
+        <div className="login-btn">
+          <i className="fa fa-user"></i>Login
+        </div>
+      </div>
     </div>
   );
 }
