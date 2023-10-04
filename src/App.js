4b9365c1f3239bb41801414837cfb357add0fe8a
Saqib Hussain <saqibhussain596@gmail.com>
2023-10-04 22:44:11 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import ExternalLink from './components/ExternalLink'; // Import the ExternalLink component
+import Burger from './components/Burger'; // Import the Burger component
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="app">
+      <Burger /> 
         <Sidebar />
         <div className="content-container">
           <Routes>
---
+++ src/components/Burger.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Burger.css"
+function Burger() {
+  return (
+    <div className="burger">
+      <div className="line1"></div>
+      <div className="line2"></div>
+    </div>
+  );
+}
+
+export default Burger;
